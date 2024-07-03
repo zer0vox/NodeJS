23d85a96277b2b3152e6d85ae4269f6aad98aae1
Sumip Chaudhary <sumip780@gmail.com>
2024-07-03 13:00:33 +0545

    3rd July Changes

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const os = require("os");
+const fs = require("fs");
+// http
+//   .createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "application/json" });
+//     const myRam = os.totalmem();
+//     const toGB = myRam / (1024 * 1024 * 1024);
+
+//     res.end(
+//       JSON.stringify({
+//         data: toGB.toFixed(2) + " GB",
+//       })
+//     );
+//   })
+//   .listen(3000);
+//use fs module to create and read the content of a file
+// fs.writeFile("pdf/file.txt", "Hello, world!", (err) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log("File created successfully!");
+
+// });
+try {
+  const data = fs.readFileSync("myfile.txt", "utf8");
+  console.log("Reading the file ", data);
+} catch (err) {
+  console.error(err);
+}
