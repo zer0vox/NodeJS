72e04a9b6b8b0eabb7b415a85292925af4b646b0
Sumip Chaudhary <sumip780@gmail.com>
2024-07-19 13:32:01 +0545

    minor changes

---
+++ new.js
@@ -0,0 +1,16 @@
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+
+const genPDF=({text,imagePath})=>{
+    const doc = new PDFDocument();
+    doc.pipe(fs.createWriteStream('output.pdf'));
+    doc.fontSize(25)
+    .text(text, 100, 100);
+    doc.image(imagePath, {
+        fit: [250, 300],
+        align: 'center',
+        valign: 'center'
+      });
+    doc.end();
+}
+genPDF({text:'Hello World',imagePath:'./image.png'});
